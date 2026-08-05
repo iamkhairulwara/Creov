93f45518cba51bdd47a6cea81e9432d26b68f052
Khair ul Wara <iamkhairulwara@gmail.com>
2026-08-05 21:35:00 +0500

    preview issue fixed

--- app/p/[slug]/route.js
+++ app/p/[slug]/route.js
@@ -87,6 +87,7 @@ export async function GET(request, { params }) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>${title}</title>
+  <link rel="icon" href="/favicon.svg" type="image/svg+xml">
   <style>
     * { box-sizing: border-box; }
     ${finalCss}
